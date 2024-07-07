3b1314b711a3f0f9b7df70c52a2ecbd45b05f4fa
Tahmid. <tahmidur.rahman.79274@gmail.com>
2024-07-07 20:43:40 +0600

    StateNotifierProvider Applied

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-final currentDate = Provider<DateTime>(
-  (ref) => DateTime.now(),
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+
+  void increment() {
+    if (state == null) {
+      state = 1;
+    } else {
+      state = state + 1;
+    }
+  }
+
+  int? get value => state;
+}
+
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter(),
 );
 
 class HomePage extends ConsumerWidget {
@@ -29,14 +54,43 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final date = ref.watch(currentDate);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Center(
-        child: Text(date.toIso8601String()),
+      appBar: AppBar(title: Consumer(
+        builder: (context, ref, child) {
+          final count = ref.watch(counterProvider);
+          return Text(count == null ? '0' : count.toString());
+        },
+      )),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextButton(
+              onPressed: () {
+                ref.read(counterProvider.notifier).increment();
+              },
+              child: Text('press'))
+        ],
       ),
     );
   }
 }
+// final currentDate = Provider<DateTime>(
+//   (ref) => DateTime.now(),
+// );
+
+// class HomePage extends ConsumerWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final date = ref.watch(currentDate);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home Page'),
+//       ),
+//       body: Center(
+//         child: Text(date.toIso8601String()),
+//       ),
+//     );
+//   }
+// }
